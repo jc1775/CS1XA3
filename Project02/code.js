febd907c0b8eae66d28b05481c8ae37a4befb75e
Joseph Calarco <jcalarco26@bell.net>
2020-03-13 23:29:05 +0000

    merged completed project2

--- Project02/code.js
+++ Project02/code.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
             });
         })
     }
-    //----------------------
+    //-----------------------
 
     /*----auto slideshow--Not Currently in use
     var pics = []
