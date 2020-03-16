860181578c1a261bdf2a9f86198737b46f71e53c
Joseph Calarco <jcalarco26@bell.net>
2020-03-16 22:09:32 +0000

    merged completed project2

--- Project02/code.js
+++ Project02/code.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     //savedstuff: headerpic" src="https://avatars1.githubusercontent.com/u/47985359?s=460&v=4 color: rgb(56, 50, 50)
+    //something interesting for later $("div.popContent").load('https://cors-anywhere.herokuapp.com/https://github.com/jc1775/python-programming/blob/master/Twitter%20Sentiment%20Analysis.ipynb iframe.render-viewer')
 
 
     //----projects card generator
@@ -19,9 +20,9 @@ $(document).ready(function() {
             });
         })
     }
-    //-----------------------
+    //----------------------
 
-    /*----auto slideshow--Not Currently in use
+    //----auto slideshow--not currently in use
     var pics = []
     var x = -1;
 
@@ -45,7 +46,7 @@ $(document).ready(function() {
             slideShow();
         }, 6000)
     }
-    //----------------------*/
+    //----------------------
 
     //----This rescales a bunch of elements and font sizes if the screen width is greater than 3840px
     function winSizeCheck() {
@@ -131,7 +132,6 @@ $(document).ready(function() {
         $("button.item").show();
     }
 
-    //----This function checks the scroll position for being at the top of the page and runs other functions if true
     function topCheck() {
         if ($(window).scrollTop() == 0) {
             $("div.blackbox").fadeIn(200);
@@ -151,8 +151,6 @@ $(document).ready(function() {
             $("#title").fadeIn(2000);
         }
     }
-    //-------------------
-
 
     function fader() {
         $("div.blackbox").fadeOut(2000);
@@ -216,7 +214,6 @@ $(document).ready(function() {
         }, 500);
     }
 
-    /*----This is not currently in use
     function msgSent() {
         var name = $("#inqName").val();
         alert(name + ", your message has been sent!");
@@ -224,7 +221,6 @@ $(document).ready(function() {
         $("#inqEmail").val("");
         $("#inqName").val("");
     }
-    //---------------------*/
 
     function projectSorter() {
         var toKeep = $(this).attr("data-actsOn");
@@ -280,7 +276,7 @@ $(document).ready(function() {
             backgroundResize()
         }
     }
-    //----Gear animation
+
     function randShift() {
         var rand = Math.random() * 70;
         var rand2 = Math.random() * 2
@@ -356,10 +352,6 @@ $(document).ready(function() {
         }
 
     }
-
-    //----------------------
-
-    //----Title animation arrays and functions
     var title1 = ["<", "h", "1", " ", "i", "d", "=", "\"", "g", "r", "e", "e", "t", "i", "n", "g", "\"", ">", " ", "H", "e", "y", "!", " "]
     var title2 = ["<", "h", "1", " ", "i", "d", "=", "\"", "g", "r", "e", "e", "t", "i", "n", "g", "\"", ">", " ", "W", "a", "a", "z", "z", "z", "z", " ", "U", "p", "p", "!", "!", "!", ".", ".", "."]
     var title3 = ["<", "h", "1", " ", "i", "d", "=", "\"", "g", "r", "e", "e", "t", "i", "n", "g", "\"", ">", " ", "W", "e", "l", "c", "o", "m", "e", " ", "t", "o", " ", "J", "o", "s", "e", "p", "h", "'", "s", " ", "W", "e", "b", " ", "C", "V", "!", " ", "<", "/", "h", "1", ">"]
@@ -402,10 +394,9 @@ $(document).ready(function() {
                 o = 0;
             }
         }
-    }
-    //------------------------------
 
-    //----This function minimizes the small red background headers
+    }
+
     function smallMinimizer() {
         if ($(this).parent().find("div.contentBox").height() == 0) {
             $(this).parent().find("div.contentBox").css("height", "auto")
@@ -422,6 +413,14 @@ $(document).ready(function() {
         }
 
     }
+
+    function autoSplit() {
+        var randInterval = Math.random() * 5000
+        var gearNum = Math.floor(Math.random() * ("div.gear").length);
+        var gearSelect = $("div.gear").eq(gearNum);
+        randShift.call(gearSelect);
+        setTimeout(function() { autoSplit() }, randInterval)
+    }
     //---------------------------------------------------------------------End of function declarations
 
     cardMaker()
@@ -436,6 +435,7 @@ $(document).ready(function() {
         backgroundResize();
         winSizeCheck();
         loadRemove();
+        autoSplit();
     }, 2000);
     //----------------------
 
@@ -449,7 +449,7 @@ $(document).ready(function() {
     $("div.close").click(overlayClose)
     $("div.minimizer").click(minimizer)
     $("div.scrollTop").click(topScroll)
-        //$("#inq").click(msgSent)
+    $("#inq").click(msgSent)
     $("div.exminbtn").click(minimizerALL)
     $("div.gear").click(randShift)
     $("div.menubtn").click(menuToggle)
